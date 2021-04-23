5ffdf23ff5e7acd5acec66593e3e991c9acef0df
John Tran <john.tran.84@live.com.au>
2021-04-24 05:49:24 +1000

    Alter for BG Images

--- client-data/js/board_Skeld.js
+++ client-data/js/board_Skeld.js
@@ -64,7 +64,7 @@ Tools.connect = function () {
 
 
 	this.socket = io.connect('', {
-		"path": window.location.pathname.split("/boards/")[0] + "/socket.io",
+		"path": window.location.pathname.split("/Skeld/")[0] + "/socket.io",
 		"reconnection": true,
 		"reconnectionDelay": 100, //Make the xhr connections as fast as possible
 		"timeout": 1000 * 60 * 20 // Timeout after 20 minutes
